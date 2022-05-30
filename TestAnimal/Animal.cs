5038a1470b828fc00c87393df8bc34139a391f3e
hadasa97 <80929211+hadasa97@users.noreply.github.com>
2022-05-30 03:16:08 +0300

    Update Animal.cs

--- TestAnimal/Animal.cs
+++ TestAnimal/Animal.cs
@@ -6,48 +6,75 @@ using System.Threading.Tasks;
 
 namespace TestAnimal
 {
+    //Creating enum for animal mood
     public enum mood
     {
         MOOD_HAPPY,
         MOOD_SCARE
     }
-     abstract class Animal
+
+    //Creating an abstract class for the animal structure
+    abstract class Animal
     {
         public Boolean mammals { get; set; }
-        public Boolean carmivorous { get; set; }
+        public Boolean carnivorous { get; set; }
 
         public mood mood;
 
+        //Creating a class builder
         public Animal(Boolean mammals, Boolean carmivorous, int mood)
         {
             this.mammals = mammals;
-            this.carmivorous = carmivorous;
+            this.carnivorous = carmivorous;
             this.mood = (mood)mood;
         }
+        //Say hello function
         public virtual void sayHello()
         {
-            Console.Write("greet people by ");
+            Console.Write(this.GetType().Name + " greet people by ");
         }
 
+        //Reaction of the animals in different moods
         public virtual void sayHello(int mood)
         {
-           Console.Write("When the mood "+ (mood)mood);
-            
+            Console.Write("When their mood " + ((mood)mood) + "-");
+
         }
-        public  Boolean isMammals()
+        //Attributes 
+        public Boolean isMammals()
         {
             return this.mammals;
         }
 
         public void setMammals(Boolean mammals)
         {
-            //this.mammals = mammals;
-            Console.WriteLine(isMammals());
+            Console.Write(this.GetType().Name);
+            if ((isMammals()) == true)
+                Console.WriteLine("s are mammalian");
+            else
+
+                Console.WriteLine("s are not mammalian");
         }
 
+        public Boolean isCarnivorous()
+        {
+            return this.mammals;
+        }
 
-
-
+        public void setCarnivorous(Boolean mammals)
+        {
+            Console.Write(this.GetType().Name);
+            if ((isMammals()) == true)
+            {
+                Console.WriteLine("s are carnivorous");
+            }
+            else
+            {
+                Console.WriteLine("s are not carnivorous");
+            }
+        }
 
     }
 }
+
+
