07238841dc4b2d7ce21da043a57d2890521fae27
hadasa97 <80929211+hadasa97@users.noreply.github.com>
2022-05-30 19:40:47 +0300

    Update Program.cs

--- TestAnimal/Program.cs
+++ TestAnimal/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,22 +10,39 @@ namespace TestAnimal
     {
         static void Main(string[] args)
         {
+            //Creating the objects
             Dog dog = new Dog(true, true, 0);
-            Cat cat = new Cat(false, false, 1);
-            dog.setMammals(true);
-            cat.setMammals(false);
-            ///////List
-            // List<Dog> dogs = new List<Dog>();
-            // dogs[0] = new Dog(true, true, 0);
-            //for (int i=0; i<dogs.Count();i++)
-            // {
-            //     dogs[i].sayHello();
-            // }
+            Cat cat = new Cat(true, true, 1);
+            Frog frog = new Frog(false, false, 1);
 
+            //Sending objects (dog, cat, frog)to functions
+            dog.setMammals(true);
+            dog.setCarnivorous(true);
             dog.sayHello();
-            cat.sayHello();
             dog.sayHello(0);
             dog.sayHello(1);
+            if ((dog.getNumberOfLegs())==4)
+              Console.WriteLine("Dogs live on land,number of legs is "+ dog.getNumberOfLegs());
+            Console.WriteLine();
+
+           
+            cat.setMammals(true);
+            cat.setCarnivorous(true);
+            cat.sayHello();
+            cat.sayHello(0);
+            cat.sayHello(1);
+            Console.WriteLine("number of legs is " + cat.getNumberOfLegs());
+            Console.WriteLine();
+
+     
+            frog.setMammals(false);
+            frog.setCarnivorous(false);
+            frog.sayHello(0);
+            frog.sayHello(1);
+            Console.WriteLine("hasGills?- "+frog.hasGills());
+            Console.WriteLine("hasLaysEggs?- "+frog.hasLaysEggs());
+            Console.WriteLine("number of legs is " + frog.getNumberOfLegs());
+           
             Console.Read();
         }
     }
